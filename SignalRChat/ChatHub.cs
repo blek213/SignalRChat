c8f50baece6328a89596169677dec72723d172e4
Andrew Romanuk <blek213@gmail.com>
2018-06-18 16:17:42 +0300

    Basic confg, copy simple chat from olderst projects

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Send", message, userName);
+        }
+
+        [Authorize]
+        public void AuthenticatedSend(string message)
+        {
+
+        }
+    }
+}
